e2fda44e8c7425b5f99994b8013c32c53c42e4cb
AlexP2508 <alexander@pumarin.se>
2024-12-11 23:16:45 +0100

    feat(security): implement CustomUserDetails and CustomUserDetailsService for authentication

---
+++ backend/RideShare/src/main/java/se/pumarin/rideshare/config/security/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package se.pumarin.rideshare.config.security;
+
+import se.pumarin.rideshare.user.User;
+import se.pumarin.rideshare.user.UserRepository;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new CustomUserDetails(user);
+    }
+}
