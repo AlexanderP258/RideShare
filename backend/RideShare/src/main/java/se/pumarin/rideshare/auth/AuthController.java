c48743bed4495396d73bb2a5f868dc3f86faf2a3
AlexP2508 <alexander@pumarin.se>
2024-12-27 18:20:49 +0100

    feat(auth-login): return userId from login & store in localStorage on frontend

--- backend/RideShare/src/main/java/se/pumarin/rideshare/auth/AuthController.java
+++ backend/RideShare/src/main/java/se/pumarin/rideshare/auth/AuthController.java
@@ -5,10 +5,10 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import se.pumarin.rideshare.config.security.CustomUserDetails;
 import se.pumarin.rideshare.config.security.JwtTokenProvider;
 import se.pumarin.rideshare.dto.LoginRequest;
 import se.pumarin.rideshare.dto.RegisterRequest;
-import se.pumarin.rideshare.dto.TokenResponse;
 import se.pumarin.rideshare.user.UserService;
 
 import java.util.Map;
@@ -33,19 +33,23 @@ public class AuthController {
     public ResponseEntity<?> register(@RequestBody RegisterRequest req) {
         userService.registerUser(req.getUsername(), req.getEmail(), req.getPassword());
         return ResponseEntity.ok(Map.of("message", "User registered successfully"));
-
     }
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequest req) {
-        
         Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(req.getUsername(), req.getPassword()));
-
+                new UsernamePasswordAuthenticationToken(req.getUsername(), req.getPassword())
+        );
 
         var userDetails = (org.springframework.security.core.userdetails.UserDetails) authentication.getPrincipal();
         String token = jwtTokenProvider.generateToken(userDetails);
 
-        return ResponseEntity.ok(new TokenResponse(token));
+        CustomUserDetails customUserDetails = (CustomUserDetails) userDetails;
+        Long userId = customUserDetails.getUser().getId();
+
+        return ResponseEntity.ok(Map.of(
+                "token", token,
+                "userId", userId
+        ));
     }
 }
